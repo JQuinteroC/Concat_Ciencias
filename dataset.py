3389f92424f1c25794eaa4acc384fe487b47580d
José Luis Quintero C <jquinteroc@acm.org>
2019-10-16 17:31:35 -0500

    Update dataset.py

--- dataset.py
+++ dataset.py
@@ -1,13 +1,21 @@
 import pandas as pd
 
 def comparacion(data):
-    for i in data.index:
-        for j in data.index:
+    i = 0
+    k = data.shape[0]
+    while i < k:
+        j = i
+        while j < k:
             if data.iat[i, 0] == data.iat[j, 0] and i!=j:
                 data.iat[i,3] = data.iat[i, 3] + data.iat[j, 3]
                 data.drop([j], inplace=True)
+                data.reset_index(drop=True, inplace=True)
+                k = k - 1
+            j = j + 1
+        i = i + 1
     return data
 
+print('PELICULAS DE BLOKBASTER')
 pelisBlokbaster = pd.DataFrame({'nombre':['Jokar', 'Harry Popoter',
                                           'Rapidos y Jugosos', 'Los Revengadores'],
                                 'existencia':[10, 23, 18, 25],
@@ -15,6 +23,7 @@ pelisBlokbaster = pd.DataFrame({'nombre':['Jokar', 'Harry Popoter',
                                 'distribuidor':['DC comacs', 'Los Wrogser',
                                                 '19 century' , 'Terrarial Studio']})
 
+print('PELICULAS DE NETFLOX')
 pelisNetflox = pd.DataFrame({'nombre' : ['Tranformistas', 'Jokar', 'Dino Park',
                                        'Piratas del Atlantico', 'Los Revengadores'],
                             'Val_Taquilla' : [5.4, 12.3, 9.3, 3.1, 6.5],
@@ -27,7 +36,7 @@ print(pelisNetflox)
 
 pelisUnion = pd.concat([pelisNetflox, pelisBlokbaster], axis = 0, join='outer',
                        join_axes = [pelisNetflox.columns], ignore_index = True)
-print(pelisUnion)
 
 comparacion(pelisUnion)
- 
+print('NUEVO INVENTARIO DE PELICULAS')
+print(pelisUnion)
